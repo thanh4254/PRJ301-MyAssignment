41248133b674aa77dd755ad63a9b18a365f9cc73
thanh4254 <165270959+thanh4254@users.noreply.github.com>
2025-10-05 23:25:09 +0700

    remove Process Request in NewServlet
    remove Process Request in NewServlet

--- MyProject/src/java/controller/servlet1.java
+++ MyProject/src/java/controller/servlet1.java
@@ -27,22 +27,8 @@ public class servlet1 extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet servlet1</title>");  
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet servlet1 at " + request.getContextPath () + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    } 
+  
+    
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /** 
@@ -55,7 +41,7 @@ public class servlet1 extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        processRequest(request, response);
+        
     } 
 
     /** 
@@ -68,7 +54,7 @@ public class servlet1 extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        processRequest(request, response);
+        
     }
 
     /** 
